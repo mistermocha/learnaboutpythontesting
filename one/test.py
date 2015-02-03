60e475ded67addcd626dc69e758970004ec039eb
Brian Weber <bweber@pinterest.com>
2015-02-02 21:13:06 -0800

    blank slate of tests

--- one/test.py
+++ one/test.py
@@ -2,18 +2,7 @@ import unittest
 from classlib import SampleClass
 
 class TestSampleClass(unittest.TestCase):
-    def setUp(self):
-        self.instance = SampleClass()
-
-    def test_init(self):
-        self.assertIsInstance(self.instance, SampleClass)
-
-    def test_default_value(self):
-        self.assertEqual(self.instance.get_value(), 'value at init')
-
-    def test_set_value(self):
-        self.instance.set_value('testing this value')
-        self.assertEqual(self.instance.get_value(), 'testing this value')
+    pass
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSampleClass)
