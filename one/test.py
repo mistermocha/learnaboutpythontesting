e44ba0c5ad3ebb1bafec2514a02a02b28bb93738
Brian Weber <bweber@pinterest.com>
2015-02-02 21:33:59 -0800

    first test

--- one/test.py
+++ one/test.py
@@ -2,7 +2,11 @@ import unittest
 from classlib import SampleClass
 
 class TestSampleClass(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.instance = SampleClass()
+
+    def test_init(self):
+        self.assertIsInstance(self.instance, SampleClass)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSampleClass)
