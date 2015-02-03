8c25f2e324582ee522b93455cabd27d6d172dfc3
Brian Weber <bweber@pinterest.com>
2015-02-02 21:54:02 -0800

    full tests

--- one/test.py
+++ one/test.py
@@ -8,6 +8,13 @@ class TestSampleClass(unittest.TestCase):
     def test_init(self):
         self.assertIsInstance(self.instance, SampleClass)
 
+    def test_default_value(self):
+        self.assertEqual(self.instance.get_value(), 'value at init')
+
+    def test_set_value(self):
+        self.instance.set_value('testing this value')
+        self.assertEqual(self.instance.get_value(), 'testing this value')
+
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSampleClass)
     unittest.TextTestRunner().run(suite)
